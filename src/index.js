47e7e8100b050a01cd753cbe4e36976312e2f66a
KUMARI ANANYA <75521183+104ananya@users.noreply.github.com>
2023-09-17 13:44:32 +0530

    destructuring

--- src/index.js
+++ src/index.js
@@ -43,46 +43,65 @@ const styleCard = {
 };
 
 const RestaurantCard = (props) => {
+  const { resData } = props;
+
+  // destructuring
+  const { imgID, resName, dish, type, rating } = resData;
+
   return (
     <div className="res-card" style={styleCard}>
-      <img src={props.imgUrl} alt="res-img" />
-      <h3>{props.resName}</h3>
-      <h4>{props.dish}</h4>
-      <p>{props.type}</p>
-      <p>{props.rating} Star</p>
+      <img
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          imgID
+        }
+        alt="res-img"
+      />
+      <h3>{resName}</h3>
+      <h4>{dish}</h4>
+      <p>{type}</p>
+      <p>{rating} Star</p>
     </div>
   );
 };
 
-const resObj = {
-  resName: "KFC Resturants",
-  dish: "Burger",
-  type: "Snacks",
-  imgUrl:
-    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/56c9ab92bd79745fd152a30fa2525426",
-  rating: "3.8",
-};
+const resList = [
+  {
+    id: 1,
+    resName: "KFC Resturants",
+    dish: "Burger",
+    type: "Snacks",
+    // imgUrl:
+    //   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/",
+    rating: "3.8",
+    imgID: "56c9ab92bd79745fd152a30fa2525426",
+  },
+  {
+    id: 2,
+    resName: "McDonald's",
+    dish: "Combo",
+    type: "Fast food",
+    rating: "4.5",
+    imgID: "ee5f8e06b300efc07c9fe3f4df40dfc4",
+  },
+  {
+    id: 3,
+    resName: "Keventer's",
+    dish: "Shakes",
+    type: "Drink",
+    rating: "4.0",
+    imgID: "f0e94c984813053f46c214f18ce73d59",
+  },
+];
 
 const Body = () => {
   return (
     <div className="body">
       <div className="search"></div>
       <div className="res-container">
-        <RestaurantCard
-          resName="XYZ Resturants"
-          dish="ABC dish"
-          type="South Indian"
-          imgUrl="https://static.toiimg.com/thumb/61879742.cms?resizemode=4&width=1200"
-          rating="4.5"
-        />
-        <RestaurantCard
-          // resName="KFC Resturants"
-          // dish="Burger"
-          // type="Snacks"
-          // imgUrl="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/56c9ab92bd79745fd152a30fa2525426"
-          // rating="3.8"
-          resData = {resObj}
-        />
+        {resList.map((restaurant) => (
+          <RestaurantCard key = {restaurant.id} resData={restaurant} />
+        ))}
       </div>
     </div>
   );
